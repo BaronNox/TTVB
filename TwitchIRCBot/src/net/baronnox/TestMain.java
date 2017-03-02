682d9771870a14c3d8df461ef8db665ac6849f2e
Jonas v. St <jonas.stowasser@stud.uni-bamberg.de>
2017-03-02 23:35:37 +0100

    +testing/debug

---
+++ TwitchIRCBot/src/net/baronnox/TestMain.java
@@ -0,0 +1,13 @@
+package net.baronnox;
+
+import net.baronnox.bot.util.Utilities;
+
+public class TestMain
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(Utilities.getDate());
+	}
+
+}
