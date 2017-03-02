558e46bb775cbfbd58c8aa1369629e554dcdd87e
Jonas v. St <jonas.stowasser@stud.uni-bamberg.de>
2017-03-02 23:35:16 +0100

    ~launch args

--- TwitchIRCBot/src/net/baronnox/Main.java
+++ TwitchIRCBot/src/net/baronnox/Main.java
@@ -1,16 +1,26 @@
 package net.baronnox;
 
+import net.baronnox.bot.AnnouncerBot;
 import net.baronnox.bot.IrcBot;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		String target = "#baronnox";
+		String target = null;
 		
+		if(args.length != 0) {
+			target = "#" + args[0];
+		} else {
+			target = "#ej_sa";
+		}
+		
+		AnnouncerBot aBot = new AnnouncerBot();
 		IrcBot bot = new IrcBot();
+		
 		bot.setVerbose(true);
 		bot.connectBot(target);
+//		aBot.joinChannelSU(target);
 	}
 
 }
